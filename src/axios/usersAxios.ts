762307331f6b34bd63ef6ad4a9d26668892d7711
xdadaya <gerakrah@gmail.com>
2023-04-19 15:08:59 +0300

    refresh tokens if get 401 response code if token is expired

--- src/axios/usersAxios.ts
+++ src/axios/usersAxios.ts
@@ -21,5 +21,39 @@ instance.interceptors.request.use(
     },
     (error) => Promise.reject(error)
 )
-  
+
+instance.interceptors.response.use(async(response) => {return response}, async(error) => {
+  const originalRequest = error.config;
+  if(error.response !== null){
+    if(error.response.status === 401 && !originalRequest._retry){
+      originalRequest._retry = true
+      try{
+        const refreshToken = getCookie("refreshToken")
+        if(!refreshToken) return Promise.reject(error);
+        const tokens = await store.dispatch('refresh', refreshToken);
+        axios.defaults.headers.common['Authorization'] = (store.state as any).auth.accessToken
+        return instance(originalRequest);
+      } catch(error: any){
+        if(error.response && error.response.data){
+            return Promise.reject(error.response.data);
+        }
+        return Promise.reject(error);
+      }
+    }
+  }
+  return Promise.reject(error)
+  }
+);
+
+const getCookie = (name: string) => {
+  const cookies = document.cookie.split('; ');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split('=');
+    if (cookie[0] === name) {
+      return cookie[1];
+    }
+  }
+  return null;
+}
+
 export default instance
